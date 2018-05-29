75167847a6feba68ddda92d739128aab3fae3f0a
GEROMAX <tobita0112@gmail.com>
2018-05-29 20:06:23 +0900

    complate js 11.2

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,26 @@
 function activateGallery() {
-  alert("hello from gallery");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      // main image set
+      let mainImage = document.querySelector("#gallery-photo > img");
+      mainImage.setAttribute("src", this.dataset.largeVersion);
+      mainImage.setAttribute("alt", this.getAttribute("alt"));
+
+      // description set
+      document.querySelector("#gallery-info > p").innerHTML = this.getAttribute(
+        "data-description"
+      );
+
+      // toggle current
+      let oldCurrent = document.querySelector("#gallery-thumbs > div.current");
+      if (oldCurrent !== null) {
+        oldCurrent.classList.remove("current");
+      }
+      this.parentNode.classList.toggle("current");
+    });
+  });
+
+  // first select
+  thumbnails[0].click();
 }
