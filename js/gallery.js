0398655bac55907db71dded2a9df023e5ed4d8ef
GEROMAX <tobita0112@gmail.com>
2018-05-29 20:28:02 +0900

    complete js 11.4

--- js/gallery.js
+++ js/gallery.js
@@ -2,19 +2,27 @@ function activateGallery() {
   let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
   thumbnails.forEach(function(thumbnail) {
     thumbnail.addEventListener("click", function() {
+      // Preload large images.
+      new Image().src = thumbnail.dataset.largeVersion;
+
       // main image set
       let mainImage = document.querySelector("#gallery-photo > img");
       mainImage.setAttribute("src", this.dataset.largeVersion);
       mainImage.setAttribute("alt", this.getAttribute("alt"));
 
       // description set
-      document.querySelector("#gallery-info > p").innerHTML = this.getAttribute(
-        "data-description"
-      );
+      document.querySelector(
+        "#gallery-info > h3"
+      ).innerHTML = this.dataset.title;
+      document.querySelector(
+        "#gallery-info > p"
+      ).innerHTML = this.dataset.description;
 
       // toggle current
       let currentClass = "current";
-      let oldCurrent = document.querySelector(`#gallery-thumbs > div.${currentClass}`);
+      let oldCurrent = document.querySelector(
+        `#gallery-thumbs > div.${currentClass}`
+      );
       if (oldCurrent !== null) {
         oldCurrent.classList.remove(currentClass);
       }
